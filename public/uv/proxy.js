74694052e6459d647277c103e4bcdbf3263d080d
xNC <133831293+NCCoder2@users.noreply.github.com>
2024-03-16 03:03:27 +0000

    Dynamic

--- public/uv/proxy.js
+++ public/uv/proxy.js
@@ -4,12 +4,47 @@ const searchEngine = document.getElementById("uv-search-engine");
 const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 
+class crypts {
+  static encode(str) {
+    return encodeURIComponent(
+      str
+        .toString()
+        .split("")
+        .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char))
+        .join("")
+    );
+  }
+
+  static decode(str) {
+    if (str.charAt(str.length - 1) === "/") {
+      str = str.slice(0, -1);
+    }
+    return decodeURIComponent(
+      str
+        .split("")
+        .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char))
+        .join("")
+    );
+  }
+}
+
+if ('serviceWorker' in navigator) {
+  var proxySetting = localStorage.getItem('proxy') || 'uv';
+  let swConfig = {
+    'uv': { file: '/uv/sw.js', config: __uv$config },
+    'dynamic': { file: '/dynamic/sw.js', config: __dynamic$config }
+  };
+
+  let { file: swFile, config: swConfigSettings } = swConfig[proxySetting];
+
+  navigator.serviceWorker.register(swFile, { scope: swConfigSettings.prefix })
+}
+
 async function handleSubmit(event) {
   event.preventDefault();
   try {
-    await registerServiceWorker;
-    const url = search(address.value, searchEngine.value);
-    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    let encodedUrl = swConfigSettings.prefix + crypts.encode(search(address.value, searchEngine.value));;
+    location.href = encodedUrl;
   } catch (err) {
     error.textContent = "Failed to register service worker.";
     errorCode.textContent = err.toString();
@@ -19,45 +54,11 @@ async function handleSubmit(event) {
 
 async function openURL(link) {
   try {
-    await registerServiceWorker;
-    const url = search(link, "https://www.google.com/search?q=%s");
-    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    let encodedUrl = swConfigSettings.prefix + crypts.encode(search(link, "https://www.google.com/search?q=%s"));;
+    location.href = encodedUrl;
   } catch (err) {
     error.textContent = "Failed to register service worker.";
     errorCode.textContent = err.toString();
     throw err;
   }
-}
-
-const registerServiceWorker = registerSW().catch((err) => {
-  error.textContent = "Failed to register service worker.";
-  errorCode.textContent = err.toString();
-  throw err;
-});
-
-form.addEventListener("submit", handleSubmit);
-
-
-if ('serviceWorker' in navigator) {
-  var proxySetting = localStorage.getItem('proxy') || 'uv';
-  let swConfig = {
-    'uv': { file: '/uv.js', config: __uv$config },
-    'dynamic': { file: '/dyn.js', config: __dynamic$config }
-  };
-
-  let { file: swFile, config: swConfigSettings } = swConfig[proxySetting];
-
-  navigator.serviceWorker.register(swFile, { scope: swConfigSettings.prefix })
-    .then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        
-        let encodedUrl = swConfigSettings.prefix + crypts.encode(search(address.value));;
-        location.href = encodedUrl;
-      });
-    })
-    .catch((error) => {
-      console.error('ServiceWorker registration failed:', error);
-    });
 }
