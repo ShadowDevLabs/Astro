a6b9e312f197d61b42c8ab89205f169e4ed81e95
xNC <133831293+NCCoder2@users.noreply.github.com>
2024-03-17 20:41:35 +0000

    Final Pushes ; Ready For Release as of 3/17. Release may and mostly will be delayed for proper bug testing & server deployment.

--- public/config/proxy.js
+++ public/config/proxy.js
@@ -1,3 +1,4 @@
+// Proxy
 const proxyDropdown = document.getElementById('proxyDropdown');
 proxyDropdown.addEventListener('change', () => {
     localStorage.setItem('proxy', proxyDropdown.value);
@@ -7,3 +8,55 @@ const proxy = localStorage.getItem('proxy');
 if (proxy) {
     proxyDropdown.value = proxy;
 }
+
+// Search Engine
+const searchEngineDropdown = document.getElementById('searchEngineDropdown');
+
+searchEngineDropdown.addEventListener('change', () => {
+    localStorage.setItem('searchEngine', searchEngineDropdown.value);
+});
+
+const searchEngine = localStorage.getItem('searchEngine');
+if (searchEngine) {
+    searchEngineDropdown.value = searchEngine;
+}
+
+
+function abtblank() {
+    const url = location.href;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+  
+    let inFrame;
+  
+    try {
+      inFrame = window !== top;
+    } catch (e) {
+      inFrame = true;
+    }
+  
+    if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+      const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+  
+      if (!popup || popup.closed) {
+        alert("Allow popups and redirects to hide this from showing up in your history.");
+      } else {
+        const doc = popup.document;
+        const iframe = doc.createElement("iframe");
+        const style = iframe.style;
+        const link = doc.createElement("link");
+  
+  
+        iframe.src = url;
+        style.position = "fixed";
+        style.top = style.bottom = style.left = style.right = 0;
+        style.border = style.outline = "none";
+        style.width = style.height = "100%";
+  
+        doc.head.appendChild(link);
+        doc.body.appendChild(iframe);
+        window.location.replace("https://google.com");
+      }
+    }
+  }
+
