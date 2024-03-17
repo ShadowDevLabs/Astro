0c5734e740370352edb3ec52f7ef28cb8a7b667b
xNC <133831293+NCCoder2@users.noreply.github.com>
2024-03-17 20:10:14 +0000

    Themes, New Logo, Setrtings

--- public/uv/proxy.js
+++ public/uv/proxy.js
@@ -43,3 +43,6 @@ async function openURL(link) {
 
 
 form.addEventListener("submit", handleSubmit);
+
+console.log('%cAstro', "color: #037ef9; font-size: 100px; font-family: 'Poppins', sans-serif; font-weight:bold;");
+
