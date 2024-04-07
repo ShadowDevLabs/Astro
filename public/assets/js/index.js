b4762cc4a2313972f39a588289b9f5f552fc044c
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2024-04-06 19:11:09 -0600

    add neon green theme

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,4 @@
-console.log('%cAstro', 'color: #037ef9; font-size: 100px; font-family: "Poppins", sans-serif; font-weight:bold;');
+console.log('%cAstro', `color: ${getComputedStyle(document.documentElement).getPropertyValue('--primary-color')}; font-size: 100px; font-family: "Poppins", sans-serif; font-weight:bold;`);
 
 if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker
     .register('/sw.js', {
--- public/assets/js/themes.js
+++ public/assets/js/themes.js
@@ -14,11 +14,13 @@ window.addEventListener('storage', (e) => {
     if (e.key === 'theme' && e.newValue) changetheme(newTheme);
 });
 
+const theme = localStorage.getItem('theme');
+changetheme(theme);
+
 document.addEventListener('DOMContentLoaded', () => {
     const theme = localStorage.getItem('theme');
 
     if (theme) {
         if (document.getElementById('themeSelector')) document.getElementById('themeSelector').value = theme;
-        changetheme(theme);
     }
 });
